714f6cec700b347d226bff53610d57f2b8524d73
yctai1994 <yctai.git@gmail.com>
2024-01-05 00:50:01 +0900

    add: Calculate Rayleigh length

--- src/GaussianOptics.jl
+++ src/GaussianOptics.jl
@@ -1,5 +1,28 @@
 module GaussianOptics
 
-greet() = print("Hello World!")
+export
+    # Types
+    GaussianBeamParams,
+    GaussianBeamAngle, 
+    GaussianBeamWaist, 
+    # Functions
+    rayleighLength,
+    # Constants
+    GBParams,
+    GBAngle,
+    GBWaist
+
+abstract type GaussianBeamParams end
+
+const GBParams = GaussianBeamParams
+
+struct GaussianBeamAngle <: GaussianBeamParams angle::Float64 end
+struct GaussianBeamWaist <: GaussianBeamParams waist::Float64 end
+
+const GBAngle = GaussianBeamAngle
+const GBWaist = GaussianBeamWaist
+
+rayleighLength(λ::Real, Θ::GaussianBeamAngle) = 1.2732395447351628 * λ / abs2(Θ.angle)
+rayleighLength(λ::Real, w::GaussianBeamWaist) = π * abs2(w.waist) / λ
 
 end # module GaussianOptics
