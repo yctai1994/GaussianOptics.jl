c5122b569e0e75c790bfd5598b9abe8151acec7a
yctai1994 <yctai.git@gmail.com>
2024-01-07 16:45:31 +0900

    update: v0.1.1

--- src/GaussianOptics.jl
+++ src/GaussianOptics.jl
@@ -6,6 +6,8 @@ export
     GaussianBeamAngle, 
     GaussianBeamWaist, 
     # Functions
+    beamWaist,
+    divergentAngle,
     rayleighLength,
     # Constants
     GBParams,
@@ -21,6 +23,26 @@ struct GaussianBeamWaist <: GaussianBeamParams waist::Float64 end
 
 const GBAngle = GaussianBeamAngle
 const GBWaist = GaussianBeamWaist
+
+#=
+                     ┌─────────────────┐
+                     │ Rayleigh Length │
+                     │        zᵣ       │
+                     └─────────────────┘
+                     ╱                 ╲
+        zᵣ = πw₀²/λ ╱                   ╲ zᵣ = 4λ/πΘ²
+                   ╱                     ╲
+  ┌────────────────┐      Θ = 2λ/πw₀     ┌─────────────────┐
+  │ Min. Beamwaist │─────────────────────│ Divergent Angle │
+  │       w₀       │─────────────────────│        Θ        │
+  └────────────────┘     w₀ = 2λ/πΘ      └─────────────────┘
+=#
+
+beamWaist(w::GaussianBeamWaist)               = w.waist
+beamWaist(λ::Real, Θ::GaussianBeamAngle)      = 0.6366197723675814 * λ / Θ.angle
+
+divergentAngle(Θ::GaussianBeamAngle)          = Θ.angle
+divergentAngle(λ::Real, w::GaussianBeamWaist) = 0.6366197723675814 * λ / w.waist
 
 rayleighLength(λ::Real, Θ::GaussianBeamAngle) = 1.2732395447351628 * λ / abs2(Θ.angle)
 rayleighLength(λ::Real, w::GaussianBeamWaist) = π * abs2(w.waist) / λ
